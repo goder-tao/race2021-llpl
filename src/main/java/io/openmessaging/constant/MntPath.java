73a15edfacd6d060563fdd3ea527a972c4d31513
Y.T.Y <912470496@qq.com>
2021-09-15 19:28:33 +0800

    改配置

--- src/main/java/io/openmessaging/constant/MntPath.java
+++ src/main/java/io/openmessaging/constant/MntPath.java
@@ -1,8 +1,8 @@
 package io.openmessaging.constant;
 
 public class MntPath {
-//    public static String AEP_PATH = "/pmem/";
-//    public static String SSD_PATH = "/essd/";
-    public static String AEP_PATH = "/mnt/sim_pmem/";
-    public static String SSD_PATH = "/home/tao/Data/sim_SSD/";
+    public static String AEP_PATH = "/pmem/";
+    public static String SSD_PATH = "/essd/";
+//    public static String AEP_PATH = "/mnt/sim_pmem/";
+//    public static String SSD_PATH = "/home/tao/Data/sim_SSD/";
 }
