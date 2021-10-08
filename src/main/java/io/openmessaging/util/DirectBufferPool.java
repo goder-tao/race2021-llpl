351ce42d689f5b28d43818b1b47ea89e5632a4bb
Y.T.Y <912470496@qq.com>
2021-10-08 11:01:32 +0800

    增大写入线程池

---
+++ src/main/java/io/openmessaging/util/DirectBufferPool.java
@@ -0,0 +1,38 @@
+package io.openmessaging.util;
+
+import io.openmessaging.constant.StorageSize;
+
+import java.nio.ByteBuffer;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ * DirectByteBuffer池，减少jvm的gc压力，并且复用ByteBuffer，
+ * 省去堆内外Buffer的复制，减少读写耗时
+ * @author tao
+ * @date 2021-10-08*/
+public class DirectBufferPool {
+    // 单例
+    private static final DirectBufferPool instance = new DirectBufferPool();
+    // 每个buffer的最大size 24K
+    private int defaultUnitSize = (int) (StorageSize.KB * 24);
+    // 用队列保存池
+    private ConcurrentLinkedQueue<ByteBuffer> pool = new ConcurrentLinkedQueue<>();
+
+    private DirectBufferPool() {
+
+    }
+
+    // 回收一个buffer
+    public void deAllocate(ByteBuffer buffer) {
+
+    }
+
+    // 分配一个buffer
+    public ByteBuffer allocate() {
+        return null;
+    }
+
+    public static DirectBufferPool getInstance() {
+        return instance;
+    }
+}
